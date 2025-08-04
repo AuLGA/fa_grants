5cf4988d6473c2fed9a814e7d46e610935773cdd
spulick <sreehari.pulickamadhom@alga.asn.au>
2025-08-04 13:25:20 +1000

    recover environment and disable render script

--- meta/fix_page_breaks.py
+++ meta/fix_page_breaks.py
@@ -1,32 +1,3 @@
-from glob import glob
-import json
+# A simple script to open all docx files in "QUARTO_PROJECT_OUTPUT_FILES" directory and add a page break before each heading.
+
 import os
-
-for file in glob("*.qmd"):
-    if os.path.exists(file):
-        with open(file, "r") as f:
-            content = f.read()
-        
-        if "{{< pagebreak >}}" not in content:
-            with open(file, "w") as f:
-                # Add page break at the end of the file
-                f.write(content + "\n{{< pagebreak >}}")
-
-for file in glob("*.ipynb"):
-    if os.path.exists(file):
-        with open(file, "r") as f:
-            content = f.read()
-        
-        if "{{< pagebreak >}}" not in content:
-            nb = json.loads(content)
-
-            nb["cells"].append({
-            "cell_type": "markdown",
-            "metadata": {},
-            "source": [
-                "{{< pagebreak >}}"
-            ]
-        })
-            
-        with open(file, "w") as f:
-            json.dump(nb, f)
