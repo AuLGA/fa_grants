1d6a381f71aeb6535aa272c8f85c942a463dd3e6
spulick <sreehari.pulickamadhom@alga.asn.au>
2025-08-05 10:42:09 +1000

    fix script

--- meta/fix_page_breaks.py
+++ meta/fix_page_breaks.py
@@ -11,20 +11,13 @@ output_files = output_dir.split("\n")
 for file in output_files:
     if not file.endswith(".docx"):
         continue
-    print(f"Processing {file}...")
 
     doc = docx.Document(file)
 
     for paragraph in doc.paragraphs:
         if paragraph.style.name.startswith("Heading 1"):
-
-            print(f"Adding page break before {paragraph.text}...")
-
-            # Insert a page break before the heading
-
             run = paragraph.insert_paragraph_before().add_run()
             run.add_break(docx.enum.text.WD_BREAK.PAGE)
             
     doc.save(file)
-    print(f"Saved {file} with page breaks added.")
 
