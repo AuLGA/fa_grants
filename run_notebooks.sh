fa4aaf184b8c110de1de2242b2660e95b98b08aa
spulick <sreehari.pulickamadhom@alga.asn.au>
2025-08-25 10:44:25 +1000

    transferring to mac

---
+++ run_notebooks.sh
@@ -0,0 +1,7 @@
+jupyter execute \
+    20_perc_min_grant.ipynb \
+    20_perc_min_grant_agg.ipynb \
+    10_perc_min_grant.ipynb \
+    10_perc_min_grant_agg.ipynb \
+    no_min_grant.ipynb \
+    no_min_grant_agg.ipynb
